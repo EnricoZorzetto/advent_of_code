51f2452a6670dce3f424d4a61c775dea73ec273a
EnricoZorzetto <zorzettoenrico@gmail.com>
2022-12-01 13:32:28 -0500

    2022 d1

---
+++ 2022/code1.py
@@ -0,0 +1,23 @@
+
+import os
+import numpy as np
+
+
+# inputfile = "test_data1.txt"
+inputfile = "data1.txt"
+
+ev = [[]]
+with open(inputfile) as f:
+    mylist = f.read().splitlines() 
+
+for l in mylist:
+    if (l==''):
+        ev.append([])
+    else:
+        ev[-1].append(int(l))
+
+sums = np.sort([np.sum(np.array(i)) for i in ev])
+print('The largest value is = {}'.format(sums[-1]))
+print('The 3 largest values are = {}; sum = {}'.format(sums[-3:], np.sum(sums[-3:])))
+
+
